78fed8843a644e30561a7dbd4ac5c61a3b365856
cyai <83634399+cyai@users.noreply.github.com>
2021-05-19 18:23:54 +0530

    Add files via upload

---
+++ rock_paper_sessor.py
@@ -0,0 +1,54 @@
+import random
+youWin = True
+
+def gamewin(comp, you):
+    
+    if comp == you:
+        return None
+    elif comp == 'r':
+        if you == 'p':
+            return True
+        elif you == 's':
+            return False
+    
+    elif comp == 'p':
+        if you == 's':
+            return True
+        elif you  == 'r':
+            return False
+    
+    elif comp == 's':
+        if you == 'r':
+            return True
+        elif you == 'p':
+            return False
+    
+
+print("Computers turn ROCK(r) PAPAER(p) or SISSORS(s)")
+comp_turn = random.randint(1,3)
+if comp_turn == 1:
+    comp = 'r'
+elif comp_turn == 2:
+    comp = 'p'
+else:
+    comp = 's'
+
+
+you = input("Your turn  ROCK(r) PAPAER(p) or SISSORS(s)?? ")
+if you == 'r' or you == 's' or you == 'p':
+    a = gamewin(comp, you)
+
+    print(f"Computer choosed {comp}")
+    print(f"YOu choosed {you}")
+
+    if a == None:
+        print("Game TIE!!!")
+
+    elif a:
+        print("You WIN!!!")
+
+    else:
+        print("You LOOSE!!")
+else:
+    print("!!INVALID INPUT!!")
+    exit
